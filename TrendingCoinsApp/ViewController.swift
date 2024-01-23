ae61d85dbeb73a730fab05afe762d8406f69f508
Vladislav <107634092+zcredi@users.noreply.github.com>
2024-01-23 17:22:37 +0300

    Initial Commit

---
+++ TrendingCoinsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendingCoinsApp
+//
+//  Created by Владислав on 23.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
